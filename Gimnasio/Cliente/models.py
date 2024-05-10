2040a658c4a0d2f2692d435e654c1968f719618a
Valentin Pereyra <valentinpereyraa.1@gmail.com>
2024-05-10 20:58:35 -0300

    feat(Cliente): creación del modelo

--- Gimnasio/Cliente/models.py
+++ Gimnasio/Cliente/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class turno(models.Model):
+
+    Turno = models.CharField(max_length=1)
+
+
+    def __str__(self) -> str:
+        return self.Turno
+    
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=1)
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=1)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+class Turno(models.Model):
+    nombre = models.PositiveIntegerField(unique=True)
+    turno = models.ForeignKey(turno, on_delete=models.SET_NULL, null=True, blank=True)
+    Profesor = models.ForeignKey(Profesor, on_delete=models.SET_NULL, null=True, blank=True)
+    Alumno = models.ManyToManyField(Alumno)
