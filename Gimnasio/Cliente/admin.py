0ebc7a16fbd85f591297e7b108cd66eff0ac0627
Valentin Pereyra <valentinpereyraa.1@gmail.com>
2024-05-14 20:54:04 -0300

    cambios

--- Gimnasio/Cliente/admin.py
+++ Gimnasio/Cliente/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 
+from django.db import models
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    turno = models.CharField(max_length=10, choices=(('Mañana', 'Mañana'), ('Tarde', 'Tarde')))
+
+    def __str__(self):
+        return self.nombre
+
 
 # Register your models here.
--- Gimnasio/core/admin.py
+++ Gimnasio/core/admin.py
@@ -1,10 +1,23 @@
-from django.contrib import admin
+#from django.contrib import admin
 
 
-from . import models
+#from . import models
+#from Alumno.models import models
+#from Profesor.models import models
 
-admin.site.register(models.Alumno)
-admin.site.register(models.Profesor)
-admin.site.register(models.turno)
+
+#admin.site.register(models.Alumno)
+#admin.site.register(models.Profesor)
+#admin.site.register(models.turno)
 #admin.site.register(models.Turno)
 # Register your models here.
+
+
+from django.contrib import admin
+from Alumno.models import Alumno
+from Profesor.models import Profesor
+from Turno.models import Turno
+
+admin.site.register(Alumno)
+admin.site.register(Profesor)
+admin.site.register(Turno)
