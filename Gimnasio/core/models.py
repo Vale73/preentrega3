25d8d90d29f909bf3ef766056bc61d0807a2eddb
Valentin Pereyra <valentinpereyraa.1@gmail.com>
2024-05-14 18:48:54 -0300

    models

--- Gimnasio/core/models.py
+++ Gimnasio/core/models.py
@@ -24,13 +24,7 @@
 
 from django.db import models
 
-class Turno(models.Model):
-    turno = models.CharField(max_length=8)
-    profesor = models.ForeignKey('Profesor', on_delete=models.CASCADE, null=True, blank=True)  # Establecer un valor predeterminado, como None
-    alumnos = models.ManyToManyField('Alumno')
 
-    def __str__(self):
-        return self.turno
 
 class Alumno(models.Model):
     nombre = models.CharField(max_length=8)
@@ -43,14 +37,17 @@ class Profesor(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class turno(models.Model):
+    nombre= models.CharField(max_length=8)
+
+    def __str__(self):
+     return self.nombre
 
 
 
-
-
-
-#class Turno(models.Model):
-#    nombre = models.PositiveIntegerField(unique=True)
-#    turno = models.ForeignKey(turno, on_delete=models.SET_NULL, null=True, blank=True)
-#    Profesor = models.ForeignKey(Profesor, on_delete=models.SET_NULL, null=True, blank=True)
-#    Alumno = models.ManyToManyField(Alumno)
+class Turno(models.Model):
+    nombre = models.PositiveIntegerField(unique=True)
+    turno = models.ForeignKey(turno, on_delete=models.SET_NULL, null=True, blank=True)
+    Profesor = models.ForeignKey(Profesor, on_delete=models.SET_NULL, null=True, blank=True)
+    Alumno = models.ManyToManyField(Alumno)
