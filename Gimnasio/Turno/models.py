a19e6033baecb1835634b0dbe9bb2d328e4df70b
Valentin Pereyra <valentinpereyraa.1@gmail.com>
2024-05-14 22:04:18 -0300

    cambios

--- Gimnasio/Turno/models.py
+++ Gimnasio/Turno/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-from Profesor.models import models
-from Alumno.models import models
+from Profesor.models import Profesor
+from Alumno.models import Alumno
 
 #class Turno(models.Model):
 #    nombre = models.CharField(max_length=100)
@@ -21,6 +21,9 @@ class Turno(models.Model):
 
     def __str__(self):
         return self.turno
+    
+
+
 
 #class Alumno(models.Model):
 #    nombre = models.CharField(max_length=8)
--- Gimnasio/core/models.py
+++ Gimnasio/core/models.py
@@ -1,28 +1,14 @@
 from django.db import models
-#from Alumno.models import models
-#from Profesor.models import models
+from Alumno.models import Alumno
+from Profesor.models import Profesor
 
- #Create your models here.
-
-class turno(models.Model):
-
+class Turno(models.Model):
     turno = models.CharField(max_length=8)
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    alumnos = models.ManyToManyField(Alumno)
 
-
-    def __str__(self) -> str:
+    def __str__(self):
         return self.turno
-    
-class Alumno(models.Model):
-    nombre = models.CharField(max_length=8)
-
-    def __str__(self) -> str:
-        return self.nombre
-    
-class Profesor(models.Model):
-    nombre = models.CharField(max_length=8)
-
-    def __str__(self) -> str:
-        return self.nombre
     
 
 
